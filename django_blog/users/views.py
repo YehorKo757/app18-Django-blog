ddf768cc3d0e79007617aa15de3452a3c4a7e7ac
YehorKo757 <108575213+YehorKo757@users.noreply.github.com>
2024-09-25 15:31:52 +0100

    Add user picture, add signal to create and update profile, when user was created or updated. Add login and logout functionality.

--- django_blog/users/views.py
+++ django_blog/users/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -10,10 +12,20 @@ def register(request):
             form.save()
             username = form.cleaned_data.get("username")
             messages.success(request,
-                             f"Account created for {username}!")
-            return redirect("blog-home")
+                             f"Your account has been created! You are now able to login")
+            return redirect("login")
     else:
         form = UserRegisterForm()
     return render(request,
                   'users/register.html',
                   context={'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/logout.html")
+
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html")
